a212e97de1a1b9eba08d6dd82208c7fe609c04b8
Luke Taylor <luke@deentaylor.com>
2016-07-22 18:30:36 -0400

    Initial commit

---
+++ placeholder/main.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+  var color = "#8ed1f4"
+  // Initialize particleground
+  particleground(document.getElementById("particles"), {
+    dotColor: color,
+    lineColor: color
+  });
+};
