faa6b69c4f4cba95f71e4532732570b475330360
The-Penultimate-Defenestrator <ldt@deentaylor.com>
2016-08-05 20:57:21 -0400

    Very basic theming
    This is more complicated than it seems like it should be...

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
 var app = angular.module("SocialNPHS", ["ngMaterial"]);
+
+app.config(function($mdThemingProvider) {
+  // Set up a dark theme using blue grey and amber colors
+  $mdThemingProvider.theme("default")
+    .primaryPalette("blue-grey")
+    .accentPalette("amber")
+    .warnPalette("deep-orange")
+    .backgroundPalette("grey")
+    .dark();
+});
