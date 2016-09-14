9647a7c9259630733b23e9e525b7a3f987d710e6
Luke Deen Taylor <luke@deentaylor.com>
2016-09-14 19:57:58 -0400

    Placeholder: add missing semicolon

--- placeholder/main.js
+++ placeholder/main.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-  var color = "#8ed1f4"
+  var color = "#8ed1f4";
   // Initialize particleground
   particleground(document.getElementById("particles"), {
     dotColor: color,
